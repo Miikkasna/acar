9ee31ec32a6aca816ed3932c5a6e161d2319fa62
Miikka Sainio <miikka.sainio@gmail.com>
2022-04-18 22:34:26 +0300

    some camera testing

--- drive.py
+++ drive.py
@@ -3,25 +3,28 @@ import time
 import cv2
 from logger import DB_logger
 
+
 # set up database logger
 log = DB_logger(batch=True, batch_size=50)
 log.set_new_testcase()
 
 # define camera
 cap = cv2.VideoCapture(0)
-
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')) # depends on fourcc available camera
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 160)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 120)
+cap.set(cv2.CAP_PROP_FPS, 100)
 
 def main():
     # init last time
     last_time = time.time()
     while True:
-        time.sleep(0.05)
+        time.sleep(0.01)
 
         # get camera frame
         ret, frame = cap.read()
         # write image to apache server
-        #cv2.imwrite('/var/www/html/image.jpg', frame)
-
+        cv2.imwrite('/var/www/html/image.jpg', frame)
 
         # log delta time
         interval = (time.time()-last_time)*1000
