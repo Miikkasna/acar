e91e7aa62695b2f4391362cac8a09a2ad1986b24
Miikka Sainio <miikka.sainio@gmail.com>
2022-04-18 18:56:32 +0300

    update drive.py with camera testing

--- drive.py
+++ drive.py
@@ -1,11 +1,32 @@
 import numpy
 import time
 import cv2
+from logger import DB_logger
 
+# set up database logger
+log = DB_logger(batch=True, batch_size=50)
+log.set_new_testcase()
+
+# define camera
+cap = cv2.VideoCapture(0)
 
 
 def main():
-    print("Hello World!")
+    # init last time
+    last_time = time.time()
+    while True:
+        time.sleep(0.05)
+
+        # get camera frame
+        ret, frame = cap.read()
+        # write image to apache server
+        #cv2.imwrite('/var/www/html/image.jpg', frame)
+
+
+        # log delta time
+        interval = (time.time()-last_time)*1000
+        last_time = time.time()
+        log.log_performance(interval)
 
 
 
