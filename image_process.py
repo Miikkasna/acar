33a66c1a42cf8be964ed00a35fc5d16445924221
Miikka Sainio <miikka.sainio@gmail.com>
2022-04-20 23:33:30 +0300

    update image process

--- image_process.py
+++ image_process.py
@@ -11,8 +11,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-image_path = 'C:\\Users\\sainmi53\\Desktop\\images1\\image{}.jpg'.format(59)
-image1 = cv2.imread(image_path)
+
 #plt.imshow(image1)
 #plt.show()
 
@@ -34,8 +33,9 @@ def region(image):
     height, width = image.shape
     #isolate the gradients that correspond to the lane lines
     triangle = np.array([
-                       [(100, height), (475, 325), (width, height)]
-                       ])
+                       [(0, height/4), (width/2, height/8), (width, height/4), (width, height), (0, height)]
+                       ]).astype('int32')
+    #print(triangle)
     #create a black image with the same dimensions as original image
     mask = np.zeros_like(image)
     #create a mask (triangle that isolates the region of interest in our image)
@@ -49,6 +49,7 @@ def display_lines(image, lines):
     if lines is not None:
         for line in lines:
             x1, y1, x2, y2 = line
+            if abs(y1-y2) < 5: continue
             #draw lines on a black image
             cv2.line(lines_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
     return lines_image
@@ -59,11 +60,11 @@ def average(image, lines):
 
     if lines is not None:
       for line in lines:
-        print(line)
+        #print(line)
         x1, y1, x2, y2 = line.reshape(4)
         #fit line to points, return slope and y-int
         parameters = np.polyfit((x1, x2), (y1, y2), 1)
-        print(parameters)
+        #print(parameters)
         slope = parameters[0]
         y_int = parameters[1]
         #lines on the right have positive slope, and lines on the left have neg slope
@@ -81,7 +82,8 @@ def average(image, lines):
     return np.array([left_line, right_line])
 
 def make_points(image, average):
-    print(average)
+    #
+    # print(average)
     slope, y_int = average
     y1 = image.shape[0]
     #how long we want our lines to be --> 3/5 the size of the image
@@ -93,28 +95,38 @@ def make_points(image, average):
 
 '''##### DETECTING lane lines in image ######'''
 
-copy = np.copy(image1)
-edges = cv2.Canny(copy,50,150)
-isolated = edges#region(edges)
-#plt.imshow(edges)
-#plt.show()
+for i in range(1, 400, 4):
+    try:
+        image_path = 'C:\\Users\\miikk\\OneDrive\\Desktop\\test2\\image{}.jpg'.format(i)
+        image1 = cv2.imread(image_path)
 
-plt.imshow(isolated)
-plt.show()
+        copy = np.copy(image1)
+        edges = cv2.Canny(copy,250,100)
+        isolated = region(edges)
+        #plt.imshow(edges)
+        #plt.show()
 
-#cv2.waitKey(0)
+        #plt.imshow(isolated)
+        #plt.show()
+
+        #cv2.waitKey(0)
 
 
-#DRAWING LINES: (order of params) --> region of interest, bin size (P, theta), min intersections needed, placeholder array, 
-lines = cv2.HoughLinesP(isolated, 2, np.pi/180, 10, np.array([]), minLineLength=80, maxLineGap=12)
-points = []
-for line in lines:
-    points.append(line.reshape(4))
-averaged_lines = average(copy, lines)
-black_lines = display_lines(copy, points)
-#taking wighted sum of original image and lane lines image
-lanes = cv2.addWeighted(copy, 0.8, black_lines, 1, 1)
-plt.imshow(lanes)
-plt.show()
+        #DRAWING LINES: (order of params) --> region of interest, bin size (P, theta), min intersections needed, placeholder array, 
+        lines = cv2.HoughLinesP(isolated, 10, np.pi/180, 70, np.array([]), minLineLength=70, maxLineGap=10)
+        points = []
+        for line in lines:
+            points.append(line.reshape(4))
+        #averaged_lines = average(copy, lines)
+        black_lines = display_lines(copy, points)
+        #taking wighted sum of original image and lane lines image
+        lanes = cv2.addWeighted(copy, 0.8, black_lines, 1, 1)
+        plt.imshow(lanes)
+        #plt.savefig('test2\\im{}.jpg'.format(i))
+        #plt.show()
+        plt.pause(0.2)
+        plt.cla()
+    except:
+        pass
 
-#cv2.waitKey(0)
+    #cv2.waitKey(0)
