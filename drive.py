8ad08403436f892a14ed16bb6b0496bf82b3172f
Miikka Sainio <miikka.sainio@gmail.com>
2022-04-27 17:26:13 +0300

    Add encoder & speed measurement + Idle driver

--- drive.py
+++ drive.py
@@ -4,7 +4,7 @@ import cv2
 from logger import DB_logger
 import numpy as np
 from flask import Flask, Response, send_file
-from drive_control import GamePad
+from drive_control import Idle
 
 
 # set up database logger
@@ -22,29 +22,36 @@ cap.set(cv2.CAP_PROP_FPS, 60)
 #setup video stream
 import video_stream
 
+# define driver agent
+driver = Idle()
 
 def main():
     # init last time
     last_time = time.time()
-    driver = GamePad()
+    dt = 0.02 # non zero initialization
     while True:
-        time.sleep(0.01)
+        time.sleep(0.1)
 
         # get camera frame
         ret, frame = cap.read()
         # update stream
         video_stream.set_image(frame)
 
-        #set driving parameters
+        # calculate inputs
+        driver.calc_inputs(dt)
+
+        # set driving parameters
         steering = driver.get_steering()
         throttle = driver.get_throttle()
         driver.steer(steering)
         driver.throttle(throttle)
         
         # log delta time
-        interval = (time.time()-last_time)*1000
+        dt = (time.time()-last_time)
         last_time = time.time()
-        log.log_performance(interval)
+        log.log_performance(dt*1000) # save as milliseconds
+
+
 
 
 if __name__ == "__main__":
--- drive_control.py
+++ drive_control.py
@@ -1,15 +1,18 @@
 import RPi.GPIO as GPIO
 import xbox
+import time
 
-pwm_freq = 200
-servo_pin = 18
-motor_pin = 17
-
-
-servo_pin = 16			# PWM pin connected
-mcu_pin = 18		# PWM pin connected
+# setup GPIO
 GPIO.setwarnings(False)			#disable warnings
 GPIO.setmode(GPIO.BOARD)		#set pin numbering system
+
+# define GPIO board pins
+enc_pin = 15
+servo_pin = 16			# PWM pin connected
+mcu_pin = 18		# PWM pin connected
+
+# define PWM setup
+pwm_freq = 200
 GPIO.setup(servo_pin,GPIO.OUT)
 servo_pwm = GPIO.PWM(servo_pin,pwm_freq)		#create PWM instance with frequency
 servo_pwm.start(0)				#start PWM of required Duty Cycle
@@ -17,12 +20,22 @@ GPIO.setup(mcu_pin,GPIO.OUT)
 mcu_pwm = GPIO.PWM(mcu_pin,pwm_freq)		#create PWM instance with frequency
 mcu_pwm.start(0)				#start PWM of required Duty Cycle 
 
+# define encoder interrupt event
+GPIO.setup(enc_pin, GPIO.IN)
+counter = 0
+def rotation_decode(enc_pin):
+    global counter
+    if GPIO.input(enc_pin)==1:
+        counter += 1
+GPIO.add_event_detect(enc_pin, GPIO.RISING, callback=rotation_decode)
 
 def map(x, in_min, in_max, out_min, out_max):
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
 class Driver():
+    def __init__(self):
+        self.speed = 0
     def calc_pulse_width(self, control):
         return map(control, -1, 1, 950e-6, 2000e-6)
 
@@ -46,6 +59,13 @@ class Driver():
         dc = self.calc_duty_cycle(pw)
         self.set_mcu(dc)
 
+    def calc_inputs(self, dt):
+        global counter
+        distance = counter/357.0 # calibrated as meters
+        self.speed = distance / dt
+        counter = 0
+
+
 class GamePad(Driver):
     def __init__(self):
         self.joy = xbox.Joystick()
@@ -58,3 +78,13 @@ class GamePad(Driver):
     def get_throttle(self):
         _, throttle = self.joy.rightStick()
         return throttle
+
+class Idle(Driver):
+    def __init__(self):
+        print('Idle selected')
+
+    def get_steering(self):
+        return 0
+
+    def get_throttle(self):
+        return 0
